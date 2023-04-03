f3be6adf3d5bd58400193f6576870de29056d455
Bram Eichelsheim <532431@student.glu.nl>
2023-04-03 13:49:40 +0200

    WIP 1:49pm

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 80;
 const path = require('path')
-app.use(express.static(path.join(__dirname, 'onsenui-pwa')));
+app.use(express.static(path.join(__dirname, 'public')));
 const server = app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
