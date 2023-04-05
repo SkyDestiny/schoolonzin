aa6b40319b515bbb20257afe6bb0f2b0573f9da5
Bram Eichelsheim <532431@student.glu.nl>
2023-04-05 12:00:29 +0200

    WIP 12:00pm

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-// VERSION: 5
+// VERSION: 6
 // 1. Save the files to the user's device
 // The "install" event is called when the ServiceWorker starts up.
 // All ServiceWorker code must be inside events.
